7222f485e930cc87c3e4adb82d1a78ebca38c0f5
Sarang P M <sarang.pm@cabotsolutions.onmicrosoft.com>
2023-07-29 02:05:48 +0530

    feat: added patient list

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,10 @@ import ProfileBox from "./components/ProfileBox";
 import Home from "./components/home/index";
 import Layout from "./components/layout/index";
 
-import data1 from "./data/0724714f-947b-6cab-ee85-b0de7a9ad3f7";
-
+import { getPatientList } from "./get/data";
 
 function App() {
-  console.log('data1::', data1);
+  console.log("data1::", getPatientList());
   return (
     <BrowserRouter>
       <Routes>
