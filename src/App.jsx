58cd12678c31a6090efbb889ed8f22c9b921d12c
Sarang P M <sarang.pm@cabotsolutions.onmicrosoft.com>
2023-07-28 22:08:51 +0530

    feat: removed unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import ProfileBox from "./components/ProfileBox";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
