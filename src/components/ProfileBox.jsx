053d922c4a3c43111c021fc9e46c0a9e764cab24
Anand ks <anand.ks@cabotsolutions.com>
2023-07-28 22:03:57 +0530

    feat : profile box

---
+++ src/components/ProfileBox.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Box, CardActionArea } from "@mui/material";
+import "./ProfileBox.css";
+function ProfileBox() {
+  return (
+    <div>
+      <div style={{ color: "green", width: "250px", background:'red' , height:'250px' }}>
+
+
+      </div>
+    </div>
+  );
+}
+
+export default ProfileBox;
