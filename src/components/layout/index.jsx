b1bdfa1d6060e37a98a8a557cc5c11a8dfbcb8a6
Sarang P M <sarang.pm@cabotsolutions.onmicrosoft.com>
2023-07-29 01:08:32 +0530

    feat: added layout

---
+++ src/components/layout/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Outlet, useLocation } from "react-router-dom";
+function Layout() {
+  const location = useLocation();
+
+  return (
+    <div>
+      <div
+        style={{
+          width: "100%",
+          height: "40px",
+          backgroundColor: "#5697C6",
+          position: "absolute",
+          top: 0,
+          left: 0,
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <div style={{ color: "white", marginLeft: "10px" }}>
+          {location.pathname.replace("/", "")}
+        </div>
+      </div>
+      <Outlet />
+    </div>
+  );
+}
+
+export default Layout;
