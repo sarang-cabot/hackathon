7bb6cc613b725156f6d4f403db18eff99e7af344
Sarang P M <sarang.pm@cabotsolutions.onmicrosoft.com>
2023-07-28 22:13:16 +0530

    feat: added react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ProfileBox from "./components/ProfileBox";
+import Home from "./components/home/index";
+
+const router = createBrowserRouter([
+  {
+    path: "/profile",
+    element: <ProfileBox/>,
+  },
+  {
+    path: "/",
+    element: <Home/>,
+  },
+]);
 
 function App() {
-
-  return (
-    <>
-      <div>
-        <ProfileBox></ProfileBox>
-      </div>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/components/home/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <div>Home page</div>
+      <div>Landing page views gies here..</div>
+    </div>
+  );
+}
+
+export default Home;
