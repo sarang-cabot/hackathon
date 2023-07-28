c0abccd8a277ea0fa6c2f6bd818f14e7f9b482af
Sarang P M <sarang.pm@cabotsolutions.onmicrosoft.com>
2023-07-29 01:54:11 +0530

    feat: added demo data

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import ProfileBox from "./components/ProfileBox";
 import Home from "./components/home/index";
 import Layout from "./components/layout/index";
 
+import data1 from "./data/0724714f-947b-6cab-ee85-b0de7a9ad3f7";
+
+
 function App() {
+  console.log('data1::', data1);
   return (
     <BrowserRouter>
       <Routes>
